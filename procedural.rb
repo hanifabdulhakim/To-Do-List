0549095bec23d2dfa38eb1ba99e97f4ecc58e893
hanifabdulhakim <hanifabdulhakim5798@gmail.com>
2018-10-02 16:22:16 +0700

    fix bug

--- procedural.rb
+++ procedural.rb
@@ -12,48 +12,64 @@ def main_loop
 			3. keluar
 			menu
 
+		p "\n"
+
 		i = g.to_i
 		case i
 		when 1
 			buat_tugas
 		when 2
 			p 'file yg sudah ada :'
+			p "\n"
 			perlihatkan_semua_tugas
-			p 'pilih nama file nya apa?'
+			p "\n"
+			p 'pilih nama file nya apa? (wajib menggunakan .txt extension)'
 			file = g.chomp
+			p "\n"
+			p 'tugas d dalam file ini adalah : '
 			lihat_tugas(file)
+			p "\n"
 		when 3
 			break
 		end
 	end
 end
 
+
 def buat_tugas
 	p 'masukan tugas yg ingin d lakukan?'
 	t = g.chomp
+	p "\n"
 	p 'apakah ingin d masukkan ke dalam file? (yes/no)'
 	o = g.chomp
+	p "\n"
 	case o
 	when /yes/
-		p 'apa nama file nya?'
+		p 'apa nama file nya? (wajib menggunakan .txt extension)'
 		f = g.chomp
-		buat_file(f,t)
+		fixed_filename = check_extension(f)
+		buat_file(fixed_filename,t)
+		p "\n"
 	when /no/
 		p 'tugas anda adalah #{t}'
 	end
 end
 
+
 def buat_file(nama_file, tugas)
 	File.open(nama_file, "w") { |file| file.puts tugas }
 end
+
 
 def lihat_tugas(a)
 	File.open(a, "r") { |file| file.each_line {|line|puts line}  }
 end
 
+
 def perlihatkan_semua_tugas
 	Dir.glob("*.txt").each { |file| puts file  }
 end
+
 
 def simpan_ke_dalam_file(filename, tugas)
 	if File.exist? filename
@@ -68,6 +84,15 @@ def simpan_ke_dalam_file(filename, tugas)
 	else
 		buat_file(filename, tugas)
 	end
+end
+
+def check_extension(filename)
+	accepted_extension = '.txt'
+ 	if filename.include? accepted_extension
+ 		filename
+ 	else
+ 		"#{filename + accepted_extension}"
+ 	end
 end
 
 main_loop
